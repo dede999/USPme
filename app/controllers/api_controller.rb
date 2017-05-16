cce86a71f9590b1a1264907d6ecdf5cacc4b80c1
dede999 <andreluizsa00@gmail.com>
2017-05-16 21:51:09 +0000

    Metodo index retorna liata dos cursos

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,10 @@
 class ApiController < ApplicationController
   def index
+    respond_to do |format|
+      msg = { :status => "ok", :message => "Success!", :html => "<b>...</b>" }
+      format.json  { render :json => Course.all }
+      format.html{}
+    end
   end
 
   def debug
