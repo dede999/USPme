1281d27d7c36ffed524bf52991efa434d3279d22
dede999 <andreluizsa00@gmail.com>
2017-05-16 19:20:33 +0000

    Relacionamento cruso e grade

--- db/migrate/20170516191727_add_course_to_grades.rb
+++ db/migrate/20170516191727_add_course_to_grades.rb
@@ -1,3 +1,4 @@
+# rails g migration AddCourseToGrades course:references
 class AddCourseToGrades < ActiveRecord::Migration
   def change
     add_reference :grades, :course, index: true, foreign_key: true
